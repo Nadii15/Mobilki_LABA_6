3255a768ce3330df405778349d62fe16fc2a337e
Grisha <grisha.zhivot@bk.ru>
2025-10-20 18:26:34 +0300

    ClassWork

---
+++ src/characters/Enemy.kt
@@ -0,0 +1,27 @@
+package characters
+
+class Enemy(
+    val name: String,
+    var hp: Int,
+    val element: String,
+
+) {
+    fun takeDamage(amount: Int) {
+        hp -= amount
+        if (hp < 0) hp = 0
+        println("$name получает урон: $amount. Осталось HP: $hp");
+    }
+
+    fun isStrong(): Boolean {
+        return hp > 100
+    }
+
+    fun getThreatLevel(): String {
+        if (hp < 0) return "Некорректное здоровье"
+        return when {
+            hp <= 50 -> "Низкий"
+            hp <= 150 -> "Средний"
+            else -> "Высокий"
+        }
+    }
+}
